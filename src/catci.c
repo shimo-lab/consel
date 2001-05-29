101a82b537474571c802370503905335dfb8a870
shimosan <shimo@sigmath.es.osaka-u.ac.jp>
2001-05-29 06:37:24 +0000

    option no_au and no_bp

--- src/catci.c
+++ src/catci.c
@@ -2,7 +2,7 @@
 
   catci.c : cat ci-files
 
-  Time-stamp: <2001-05-05 15:24:02 shimo>
+  Time-stamp: <2001-05-07 16:36:33 shimo>
 
   shimo@ism.ac.jp 
   Hidetoshi Shimodaira
@@ -17,22 +17,26 @@
 #include <math.h>
 #include "misc.h"
 
-static const char rcsid[] = "$Id$";
+static const char rcsid[] = "$Id: catci.c,v 1.1 2001/05/05 09:09:09 shimo Exp shimo $";
 
 char *fext_ci = ".ci";
 
 void byebye() {error("error in command line");}
-
+void repchar(char c, int n) { int i; for(i=0;i<n;i++) putchar(c);}
+  
 int sw_verpose=0;
+int sw_au=1;
+int sw_bp=1;
 
 int main(int argc, char** argv)
 {
   /* working variables */
-  int i,j,ifile,nfile,cm;
+  int i,j,ifile,nfile,cm,w,w2;
   FILE *fp;
   char *fname;
   double *alphavec=NULL;
   double **cimat=NULL, **semat=NULL, **eimat=NULL;
+  double **ci0mat=NULL, **se0mat=NULL, **ei0mat=NULL;
   int nalpha;
   int *orderv; double *obsvec; /* auxiliary info */
   char **fnamev;
@@ -52,6 +56,10 @@ int main(int argc, char** argv)
       i+=1;
     } else if(streq(argv[i],"-v")) {
       sw_verpose=1;
+    } else if(streq(argv[i],"--no_au")) {
+      sw_au=0;
+    } else if(streq(argv[i],"--no_bp")) {
+      sw_bp=0;
     } else byebye();
   }
 
@@ -66,25 +74,57 @@ int main(int argc, char** argv)
     cimat=fread_mat(fp,&cm,&nalpha); 
     semat=fread_mat(fp,&cm,&nalpha);
     eimat=fread_mat(fp,&cm,&nalpha);
+    ci0mat=fread_mat(fp,&cm,&nalpha); 
+    se0mat=fread_mat(fp,&cm,&nalpha);
+    ei0mat=fread_mat(fp,&cm,&nalpha);
     fclose(fp);
 
+    printf("\n#"); repchar(' ',17);
+    w=nalpha*(sw_verpose?17:7);
+    w2=w/2-2;
+    if(sw_au) {
+      repchar('-',w2); printf(" au "); repchar('-',w-w2-4);
+    }
+    printf(" |");
+    if(sw_bp) {
+      repchar('-',w2); printf(" bp "); repchar('-',w-w2-4);
+    }
     printf("\n# %4s %4s","rank","item");
     printf(" %6s","obs");
-    for(j=0;j<nalpha;j++) {
-      printf(" %6.3f",alphavec[j]);
-      if(sw_verpose) printf(" %4s %4s","se","ei");
+    if(sw_au) {
+      for(j=0;j<nalpha;j++) {
+	printf(" %6.3f",alphavec[j]);
+	if(sw_verpose) printf(" %4s %4s","se","ei");
+      }
+    }
+    printf(" |");
+    if(sw_bp) {
+      for(j=0;j<nalpha;j++) {
+	printf(" %6.3f",alphavec[j]);
+	if(sw_verpose) printf(" %4s %4s","se","ei");
+      }
     }
     for(i=0;i<cm;i++) {
       printf("\n# %4d %4d",i+1,orderv[i]+1);
       printf(" %6.1f",obsvec[i]);
-      for(j=0;j<nalpha;j++) {
-	printf(" %6.1f",cimat[i][j]);
-	if(sw_verpose) printf(" %4.1f %4.1f",semat[i][j],eimat[i][j]);
+      if(sw_au){
+	for(j=0;j<nalpha;j++) {
+	  printf(" %6.1f",cimat[i][j]);
+	  if(sw_verpose) printf(" %4.1f %4.1f",semat[i][j],eimat[i][j]);
+	}
+      }
+      printf(" |");
+      if(sw_bp){
+	for(j=0;j<nalpha;j++) {
+	  printf(" %6.1f",ci0mat[i][j]);
+	  if(sw_verpose) printf(" %4.1f %4.1f",se0mat[i][j],ei0mat[i][j]);
+	}
       }
     }
     printf("\n");
     free_vec(alphavec);
     free_mat(cimat); free_mat(semat); free_mat(eimat);
+    free_mat(ci0mat); free_mat(se0mat); free_mat(ei0mat);
     free_ivec(orderv); free_vec(obsvec);
   }
   return 0;
